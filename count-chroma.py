ef88c8857f83e2a9454fdf08fc94ce784edac254
safaet <safaetjaman@gmail.com>
2026-09-21 12:49:33 +0600

    add contributing file

--- count-chroma.py
+++ count-chroma.py
@@ -1,6 +1,6 @@
 import chromadb
 
-client = chromadb.PersistentClient(path="/app/chroma_db")
+client = chromadb.PersistentClient(path="chroma_db/")
 collections = client.list_collections()
 print(f"Total collections: {len(collections)}\n")
 for c in collections:
